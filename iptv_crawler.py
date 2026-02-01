c18842b3d2aad3678258ed43d9b32b7ee441b386
buyu10086 <1753462733@qq.com>
2026-02-01 10:39:18 +0800

    Update iptv_crawler.py

--- iptv_crawler.py
+++ iptv_crawler.py
@@ -16,12 +16,9 @@ from typing import Tuple, List, Dict, Optional
 IPTV_SOURCE_URLS = [
     "https://raw.githubusercontent.com/kakaxi-1/zubo/refs/heads/main/IPTV.txt",
     "https://raw.githubusercontent.com/kakaxi-1/IPTV/refs/heads/main/ipv4.txt",
-    "https://raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv4.m3u",
     "https://raw.githubusercontent.com/8080713/iptv-api666/refs/heads/main/output/result.m3u",
-    "https://raw.githubusercontent.com/Guovin/iptv-api/refs/heads/gd/output/result.m3u",
-    "https://raw.githubusercontent.com/BurningC4/Chinese-IPTV/master/TV-IPV4.m3u",
-    "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u",
-    "https://raw.githubusercontent.com/audyfan/tv/refs/heads/main/live.m3u"
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u",
+    "https://gh-proxy.com/raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv4.m3u"
 ]
 
 # 2. 效率核心配置
